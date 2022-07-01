be21944183103e14a427cfb43196f5d052a4c4c6
Jacob <jacob.d.wencloff@gmail.com>
2022-07-01 12:51:44 -0700

    Moved from GLFW to Pygame in preperation for A* animations

--- UI/ui.py
+++ UI/ui.py
@@ -1,9 +1,14 @@
 import random
+import pygame
 import OpenGL.GL as gl
 import imgui
 from imgui.integrations.glfw import GlfwRenderer
 import glfw 
 from array import array
+
+
+# from UI.a_star import *
+
 
 
 
@@ -42,7 +47,7 @@ class UI():
 
 
 
-        window_size = glfw.get_window_size(self.window)
+        window_size = self.window.get_size()
         imgui.set_next_window_position(0,0)
         imgui.set_next_window_size(window_size[0], 200)
 
@@ -89,6 +94,8 @@ class UI():
             if imgui.button("Quick-Sort"):
                 self.algo_to_display = "Quick"
                 imgui.close_current_popup()
+            if imgui.button("A*"):
+                self.algo_to_display = "a_star"
             imgui.end_popup()
 
         imgui.end_child()
@@ -115,6 +122,7 @@ class UI():
             imgui.set_next_window_position(300, curr_size[1])
             self.quick_sort.draw(dt)
 
+     
 
 class Editor():
     def __init__(self, window):
@@ -131,7 +139,7 @@ class Editor():
 
 
     def draw(self):
-        window_size = glfw.get_window_size(self.window)
+        window_size = self.window.get_size()
         imgui.begin("Editor")
 
         imgui.push_id("0")
@@ -193,7 +201,7 @@ class Histo_Graph():
 
     def draw(self, dt):
         self.curr_size = imgui.core.get_window_size()
-        window_size = glfw.get_window_size(self.window)
+        window_size = self.window.get_size()
         # imgui.set_next_window_position(edit_x, gui_y) 
         # imgui.set_next_window_size(window_size[0]-300, window_size[1]-200)
 
@@ -286,7 +294,7 @@ class Quick_Sort():
 
     def draw(self, dt):
         self.curr_size = imgui.core.get_window_size()
-        window_size = glfw.get_window_size(self.window)
+        window_size = self.window.get_size()
 
 
         if self.start:
@@ -304,40 +312,37 @@ class Quick_Sort():
         "",
         self.arr_copy,
         overlay_text="Quick Sort",
-        # offset by one item every milisecond, plot values
-        # buffer its end wraps around
+
         graph_size=(window_size[0]-320, window_size[1]-230),
      )
         imgui.end()
 
     def partition(self, l, r, nums):
-    # Last element will be the pivot and the first element the pointer
+
         pivot, ptr = nums[r], l
         for i in range(l, r):
             if nums[i] <= pivot:
-                # Swapping values smaller than the pivot to the front
+
                 self.keys.append([i,nums[ptr]])
                 self.keys.append([ptr, nums[i]])
                 nums[i], nums[ptr] = nums[ptr], nums[i]
                 ptr += 1
-        # Finally swapping the last element with the pointer indexed number
+
         self.keys.append([ptr, nums[r]])
         self.keys.append([r, nums[ptr]])
         nums[ptr], nums[r] = nums[r], nums[ptr]
         return ptr
  
-    # With quicksort() function, we will be utilizing the above code to obtain the pointer
-    # at which the left values are all smaller than the number at pointer index and vice versa
-    # for the right values.
+
     
     
     def quicksort(self, l, r, nums):
-        if len(nums) == 1:  # Terminating Condition for recursion. VERY IMPORTANT!
+        if len(nums) == 1:  
             return nums
         if l < r:
             pi = self.partition(l, r, nums)
-            self.quicksort(l, pi-1, nums)  # Recursively sorting the left values
-            self.quicksort(pi+1, r, nums)  # Recursively sorting the right values
+            self.quicksort(l, pi-1, nums)  
+            self.quicksort(pi+1, r, nums) 
           
         return nums
 
@@ -345,14 +350,12 @@ class Quick_Sort():
         
         for i in range(0, self.data_size):
             self.arr.append(random.randint(self.range_bot,self.range_top))
+
+class Draw_A_star():
+    def __init__(self):
+        pass
+
+
     
-    # example = [4, 5, 1, 2, 3]
-    # result = [1, 2, 3, 4, 5]
-    # print(quicksort(0, len(example)-1, example))
-    
-    # example = [2, 5, 6, 1, 4, 6, 2, 4, 7, 8]
-    # result = [1, 2, 2, 4, 4, 5, 6, 6, 7, 8]
-    # # As you can see, it works for duplicates too
-    # print(quicksort(0, len(example)-1, example))
 
         
--- main.py
+++ main.py
@@ -1,73 +1,84 @@
 from multiprocessing.spawn import import_main_path
-import glfw
+# import glfw
+import pygame
 import OpenGL.GL as gl
 import imgui
-from imgui.integrations.glfw import GlfwRenderer
+from imgui.integrations.pygame import PygameRenderer
+import sys
 from UI.ui import *
 
 
 def main():
+    pygame.init()
+    display_info = pygame.display.Info()
+    w, h = display_info.current_w, display_info.current_h
+    screen = pygame.display.set_mode((w , h), pygame.DOUBLEBUF | pygame.OPENGL | pygame.FULLSCREEN, 0, 0)
     imgui.create_context()
-    window = glfw_init()
-    impl = GlfwRenderer(window)
-    last_frame_time = glfw.get_time()
-    nav = UI(window)
+    impl = PygameRenderer()
 
-    while not glfw.window_should_close(window):
-        glfw.poll_events()
-        impl.process_inputs()
-        now = glfw.get_time()
-        dt = now - last_frame_time
-        last_frame_time = now
+    io = imgui.get_io()
+    io.display_size = (w, h)
+    last_frame_time = pygame.time.get_ticks()
+    nav = UI(screen)
+
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            impl.process_event(event)
 
         imgui.new_frame()
-        #UI is here
+        now = pygame.time.get_ticks()
+        dt = (now - last_frame_time) / 1000
+        last_frame_time = now
+
+    #     imgui.new_frame()
+    #     #UI is here
 
         # ui()
         nav.draw(dt)
-
-
 
         gl.glClearColor(.3, .3, .3, 1)
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
 
         imgui.render()
         impl.render(imgui.get_draw_data())
-        glfw.swap_buffers(window)
+        pygame.display.flip()
 
-    impl.shutdown()
-    glfw.terminate()
+    # impl.shutdown()
+    # glfw.terminate()
 
 
-def glfw_init():
-    if not glfw.init():
-        print('Could not initialize OpenGl context')
-        exit(1)
+# def glfw_init():
+#     if not glfw.init():
+#         print('Could not initialize OpenGl context')
+#         exit(1)
 
-    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
-    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
-    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+#     glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
+#     glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
+#     glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
 
-    glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, gl.GL_TRUE)
+#     glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, gl.GL_TRUE)
 
-    monitor = glfw.get_primary_monitor()
+#     monitor = glfw.get_primary_monitor()
 
-    video_mode = glfw.get_video_mode(monitor)
+#     video_mode = glfw.get_video_mode(monitor)
 
-    width, height = video_mode.size.width, video_mode.size.height
-    window_name = 'test'
+#     width, height = video_mode.size.width, video_mode.size.height
+#     window_name = 'test'
     
-    window = glfw.create_window(
-        int(width) , int(height), window_name, monitor, None 
-    )
-    glfw.make_context_current(window)
+#     window = glfw.create_window(
+#         int(width) , int(height), window_name, monitor, None 
+#     )
+#     glfw.make_context_current(window)
 
-    if not window:
-        glfw.terminate()
-        print('Could not initialize')
-        exit(1)
+#     if not window:
+#         glfw.terminate()
+#         print('Could not initialize')
+#         exit(1)
 
-    return window
+#     return window
 
 
 
